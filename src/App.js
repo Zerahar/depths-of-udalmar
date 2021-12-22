93c3940201a55b01ccaf3792a58e720851e63da8
Zerahar <heinisaastamoinen@gmail.com>
2021-12-22 10:15:33 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import { Link, Outlet } from "react-router-dom";
 import { Container, Navbar, Row, Col, Nav } from "react-bootstrap"
+import header_bg from "./img/header_bg.jpg"
+import side_bg from "./img/side_bg.jpg"
 
 export default function App() {
-  const storynumber = 1
   return (
-    <Container className="full-container p-0" fluid>
-      <Navbar className="mt-5 mb-5" id="title-bar">
-        <Navbar.Brand className="nav-title w-100"><Link to="/front">Ruins of Uldamar</Link></Navbar.Brand>
+    <Container className="full-container" fluid>
+      <Navbar className="mb-5" id="title-bar" style={{ backgroundImage: `url(${header_bg})` }}>
+        <div className="nav-title w-100"><Link to="/front">Ruins of Udalmar</Link></div>
         <Nav>
-          <Link to="/characterCreation" className="nav-item">Create a character</Link>
-          <Link to="/login" className="nav-item">Login</Link>
-          <Link to={`/story/text/${storynumber}`} className="nav-item">Begin story</Link>
+          <Link to="/characterCreation" className="nav-item btn btn-dark">Create a character</Link>
+          <Link to="/login" className="nav-item btn btn-dark">Login</Link>
+          <Link to={`/story/text/1`} className="nav-item btn btn-dark">Begin story</Link>
         </Nav>
       </Navbar>
-      <Container fluid="lg" className="text-container">
+      <Container fluid="lg">
         <Row>
           <Col lg="3" className="sidebar">
-            <div className="profile-pic"></div>
-            <h3>Matthias Ironbeard</h3>
+            <div class="inner-container">
+              <div className="profile-pic"></div>
+              <h3>Matthias Ironbeard</h3>
+            </div>
           </Col>
-          <Col className="text-container p-3">
-            <Outlet />
-          </Col>
+          <Col className="text-container">
+            <div class="inner-container">
+              <Outlet />
+            </div></Col>
         </Row>
       </Container>
     </Container>
